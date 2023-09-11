6f0dc9b14f9e2ec198654e50f261376589f5c631
MartinRomanVillalba <romantincho@gmail.com>
2023-09-10 21:39:05 -0300

    primer parte del sprint 2

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/index.html"));
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/productDetail.html"));
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/productCart.html"));
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login-register.html"));
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando en el puerto ${port}`);
+});
