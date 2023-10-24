88998e7157fb57179c1b2a0dd24188f06a6a318e
MartinRomanVillalba <romantincho@gmail.com>
2023-10-24 19:55:00 -0300

    Borre unas lineas

--- src/routes/products.js
+++ src/routes/products.js
@@ -5,16 +5,13 @@ const productsController = require('../controllers/productsController');
 const router = express.Router();
 
 // router.get('/products', )
-// router.get('/products/create', )
+router.get('/products/create', productsController.create)
 // router.get('/products/:id', )
 // router.post('/products', )
 // router.get('/products/:id/edit', )
 // router.put('/products/:id', )
 // router.delete('/products/:id', )/
 
-router.get('/productDetail', productsController.productDetail)
-router.get('/productCart', productsController)
-router.get('/productCreate', productsControllerController.productCreate)
-router.get('/productEdit', productsControllerController.productEdit)
+
 
 module.exports = router;
