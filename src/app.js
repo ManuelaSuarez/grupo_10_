dad911dafc2b676f7a053538db2a9a4015460baf
MartinRomanVillalba <romantincho@gmail.com>
2023-11-08 15:45:30 -0300

    instalacion de dependencias

--- src/app.js
+++ src/app.js
@@ -3,11 +3,12 @@ const path = require("path");
 const app = express();
 const port = 3000;
 const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
-// app.use(cookieParser())
+app.use(cookieParser())
 app.use(methodOverride('_method'))
 
 app.set('view engine', 'ejs')
