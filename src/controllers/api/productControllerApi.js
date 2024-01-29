cc9910159a61d53fb64aa4696020e5b465687e0e
ManuelaSuarez <suarezzmanuela@gmail.com>
2024-01-29 17:57:01 -0300

    haciendo la api

---
+++ src/controllers/api/productControllerApi.js
@@ -0,0 +1,30 @@
+const db = require("../database/models");
+
+module.exports = {
+    list: async (req, res) => {
+        try {
+            const products = await db.Product.findAll();
+
+            return res.json({
+                count: products.length,
+                //countByCategory: ,
+                products: products
+            });
+        } catch (error) {
+            return res.status(500).json({ error: error.message });
+        }
+    },
+    show: async (req, res) => {
+        try {
+            const product = await db.Product.findByPk(req.params.id);
+
+            return res.json({
+                product: product
+                //
+            });
+        } catch (error) {
+            return res.status(500).json({ error: error.message });
+        }
+    }
+
+};
