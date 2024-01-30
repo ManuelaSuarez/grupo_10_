184202f03c9891dbd29c6b63a28bd2f84bba8e2d
MartinRomanVillalba <romantincho@gmail.com>
2024-01-29 23:11:44 -0300

    algunos cambios en estilos

--- src/public/js/perfil.js
+++ src/public/js/perfil.js
@@ -6,10 +6,14 @@ document.addEventListener("DOMContentLoaded", function() {
       perfilMenu.style.display = (perfilMenu.style.display === "block") ? "none" : "block";
     });
   
-    // Cierra el menú si se hace clic en cualquier lugar fuera de él
     document.addEventListener("click", function(event) {
       if (!perfilBtn.contains(event.target) && !perfilMenu.contains(event.target)) {
         perfilMenu.style.display = "none";
       }
     });
   });
+
+  function toggleMenu() {
+    var nav = document.querySelector('nav');
+    nav.classList.toggle('nav-visible');
+  }
--- src/public/js/registerValidationMiddleware.js
+++ src/public/js/registerValidationMiddleware.js
@@ -1,4 +1,4 @@
-const validator = require('validator');
+const validator = require("validator");
 
 const validateRegistration = (req, res, next) => {
   const { first_name, last_name, email, password } = req.body;
@@ -7,31 +7,38 @@ const validateRegistration = (req, res, next) => {
   const errors = {};
 
   if (!validator.isLength(first_name, { min: 2 })) {
-    errors.first_name = { msg: 'El nombre debe tener al menos 2 caracteres' };
+    errors.first_name = { msg: "El nombre debe tener al menos 2 caracteres" };
   }
 
   if (!validator.isLength(last_name, { min: 2 })) {
-    errors.last_name = { msg: 'El apellido debe tener al menos 2 caracteres' };
+    errors.last_name = { msg: "El apellido debe tener al menos 2 caracteres" };
   }
 
   if (email == "") {
-    errors.email = {msg: 'Tienes que escribir un correo electrónico'};
-  } else{
-    !validator.isEmail(email)
-    errors.email = { msg: 'El correo electrónico no es válido' }
+    errors.email = { msg: "Tienes que escribir un correo electrónico" };
+  } else {
+    !validator.isEmail(email);
+    errors.email = { msg: "El correo electrónico no es válido" };
   }
 
-  if (!validator.isLength(password, { min: 8 })) {
-    errors.password = { msg: 'La contraseña debe tener al menos 8 caracteres' };
+  if (password == "") {
+    errors.password = { msg: "Tienes que escribir una contraseña" };
+  } else {
+    !validator.isLength(password, { min: 8 });
+    errors.password = { msg: "La contraseña debe tener al menos 8 caracteres" };
   }
 
-
-  if (!avatar || !['image/jpeg', 'image/png', 'image/gif'].includes(avatar.mimetype)) {
-    errors.avatar = { msg: 'La imagen debe ser un archivo válido (JPG, JPEG, PNG, GIF)' };
+  if (
+    !avatar ||
+    !["image/jpeg", "image/png", "image/gif"].includes(avatar.mimetype)
+  ) {
+    errors.avatar = {
+      msg: "La imagen debe ser un archivo válido (JPG, JPEG, PNG, GIF)",
+    };
   }
 
   if (Object.keys(errors).length > 0) {
-    return res.render('users/register', { errors, oldData: req.body });
+    return res.render("users/register", { errors, oldData: req.body });
   }
 
   next();
